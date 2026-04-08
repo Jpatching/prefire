dffbd53afe2eda2bd98eac6aa857418d9859a919
Jpatching <patchingjoshua@gmail.com>
2026-04-08 17:32:17 +0200

    feat: add classify_instruction for Squads event mapping

--- crates/monitor/src/governance.rs
+++ crates/monitor/src/governance.rs
@@ -24,3 +24,41 @@ pub enum GovernanceEvent {
         description: String,
     },
 }
+pub fn classify_instruction(instruction_name: &str, _signature: &str) -> Option<GovernanceEvent> {
+    match instruction_name {
+        "ProposalCreate" => Some(GovernanceEvent::ProposalCreated {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "ProposalApprove" => Some(GovernanceEvent::ProposalApproved {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "ProposalActivate" => Some(GovernanceEvent::ProposalActivated {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "ConfigTransactionCreate" => Some(GovernanceEvent::ConfigChange {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "MultisigAddMember" => Some(GovernanceEvent::ConfigChange {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "VaultTransactionCreate" => Some(GovernanceEvent::VaultTransfer {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "VaultTransactionExecute" => Some(GovernanceEvent::VaultTransfer {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+        "SpendingLimitUse" => Some(GovernanceEvent::VaultTransfer {
+            multisig: Pubkey::default(),
+            description: instruction_name.to_string(),
+        }),
+
+        _ => None,
+    }
+}
